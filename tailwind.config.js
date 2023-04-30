20a9cd206a2b034c161fe7ea134575b44550437b
`Mai` <`luongvuhoangmai@gmail.com`>
2023-05-01 00:55:03 +0900

    add new project

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [`./src/index.html`],
+  theme: {
+    
+    extend: {
+
+      screens: {
+        xl2: `1350px`,
+        xl1: `1300px`,
+        lg2: `1160px`,
+        lg1: `1067px`,
+        md2: `900px`,
+        md1: `860px`,
+  
+  
+  
+      },
+    },
+  },
+  plugins: [],
+}
+
